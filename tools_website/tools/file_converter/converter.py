382f6444591d7078ba47d6ca758ec9fd3fb59ed2
Aidan Comer <alhcomer@hotmail.co.uk>
2023-05-18 14:57:24 +0100

    fixed file conversion capability

--- tools_website/tools/file_converter/converter.py
+++ tools_website/tools/file_converter/converter.py
@@ -1,6 +1,7 @@
 import aspose.words as aw
 import os
 from django.conf import settings
+from io import BytesIO
 
 
 class FileConverter:
@@ -10,18 +11,23 @@ class FileConverter:
         self.output_file = self.convert_file()
 
     def get_full_file_path(self):
-        return settings.MEDIA_ROOT + f"user_{self.user.id}/{self.input_file.name}"
+        # TODO: Check whether multiple chunks is appropriate given file validation sizes
+        if self.input_file.multiple_chunks():
+            raise ValueError("File is too large to be converted.")
+        else:
+            file_bytes = self.input_file.read()
+            return BytesIO(file_bytes)
 
     def save_path(self):
-        return settings.MEDIA_ROOT + f"user_{self.user.id}/"
+        return os.path.join(settings.MEDIA_ROOT, f'user_{self.user.id}')
 
     def convert_file(self):
         input_title = os.path.splitext(self.input_file.name)[0]
         doc = aw.Document(self.get_full_file_path())
-        output_path = self.save_path() + input_title + ".pdf"
-        #TODO: fix convert file so that it can convert to any extension
+        output_path = os.path.join(self.save_path(), input_title + '.pdf')
         doc.save(output_path)
-        return output_path
+        output_file_rel_path = os.path.relpath(output_path, settings.MEDIA_ROOT)
+        return output_file_rel_path
 
         #Documentation here : https://pypi.org/project/aspose-words/
 
--- tools_website/tools/file_converter/views.py
+++ tools_website/tools/file_converter/views.py
@@ -9,34 +9,27 @@ def about(request):
     return render(request, 'tools/file-converter/about.html')
 
 def file_converter(request):
-
-    # TODO: make it so that if file is already in db, ask whether to overwrite or not
-
-    if request.method == "POST":
+    if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
-        for field in form:
-            for error in field.errors:
-                print(error)
-
-        print(request.FILES['input_file'])
 
         if form.is_valid():
-            # TODO: grab file from request.FILES, convert file and save to db
             file_to_convert_base = form.save(commit=False)
             file_to_convert_base.user = request.user
-            # file_to_convert_base.output_file = FileConverter(request.FILES['input_file'], request).output_file
-            file_to_convert_base.output_file.name = 'user_{0}/{1}'.format(request.user.id, request.FILES['input_file'].name)
+
+            file_converter = FileConverter(request.FILES['input_file'], request)
+            file_to_convert_base.output_file.name = file_converter.output_file
+
             file_to_convert_base.save()
             form.save_m2m()
-        #TODO: get object from Form, convert file here and save. Then render page again with updates user files
-        user_files = FileConversion.objects.filter(user=request.user.id)
-        context = {}
-        context["user_files"] = user_files
-        context["form"] = FileUploadForm()
-        return render(request, 'tools/file-converter/file-converter.html', context=context)
+
+    user_files = FileConversion.objects.filter(user=request.user.id)
+    context = {
+        'user_files': user_files,
+        'form': FileUploadForm(),
+    }
+    return render(request, 'tools/file-converter/file-converter.html', context=context)
 
 def download(request, files_id):
-    #TODO: Fix so that user can only view if file id matches request.user id
     files_to_download = FileConversion.objects.get(id=files_id)
     if request.user.id == files_to_download.user.id:
         return render(request, 'tools/file_converter/<int:user_id>/<int:file_id>/download')
