ca8eca8cf2697d823c57c89773ebe0e60737cc46
Aidan Comer <alhcomer@hotmail.co.uk>
2023-05-23 14:32:03 +0100

    Conversion functionality now working fully

--- tools_website/tools/file_converter/converter.py
+++ tools_website/tools/file_converter/converter.py
@@ -2,6 +2,8 @@ import aspose.words as aw
 import os
 from django.conf import settings
 from io import BytesIO
+from .models import FileConversion
+import uuid
 
 
 class FileConverter:
@@ -22,12 +24,20 @@ class FileConverter:
         return os.path.join(settings.MEDIA_ROOT, f'user_{self.user.id}')
 
     def convert_file(self):
-        input_title = os.path.splitext(self.input_file.name)[0]
+        input_filename, input_extension = os.path.splitext(self.input_file.name)
+        output_filename = f'{input_filename}.pdf'  # Output file will have the same name as the input file, but with .pdf extension
+
+        if FileConversion.objects.filter(user=self.user, input_file__contains=input_filename).exists():
+            # If a FileConversion object with a similar input filename exists for the same user
+            unique_filename = str(uuid.uuid4()) + '_' + output_filename
+            output_filename = unique_filename
+
+        output_path = os.path.join(self.save_path(), output_filename)
+
         doc = aw.Document(self.get_full_file_path())
-        output_path = os.path.join(self.save_path(), input_title + '.pdf')
         doc.save(output_path)
+
         output_file_rel_path = os.path.relpath(output_path, settings.MEDIA_ROOT)
         return output_file_rel_path
-
         #Documentation here : https://pypi.org/project/aspose-words/
 
--- tools_website/tools/file_converter/models.py
+++ tools_website/tools/file_converter/models.py
@@ -1,3 +1,5 @@
+import os
+import uuid
 from django.db import models
 from .validators import FileValidator
 from django.conf import settings
@@ -6,30 +8,38 @@ from .mystorage import CleanFileNameStorage
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-
     return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 
-allowed_extensions=[
-    "application/msword", 
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document", 
-    "text/html", 
-    "application/vnd.oasis.opendocument.text", 
-    "application/pdf", "application/vnd.ms-powerpoint", 
-    "application/vnd.openxmlformats-officedocument.presentationml.presentation", 
-    "application/rtf", 
-    "text/plain", 
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
-    "application/pdf"]
+def get_unique_filename(instance, filename):
+    # Generate a unique filename for the output file
+    ext = os.path.splitext(filename)[1]
+    unique_filename = f'{uuid.uuid4()}{ext}'
+    return os.path.join(user_directory_path(instance, unique_filename))
+
+
+allowed_extensions = [
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "text/html",
+    "application/vnd.oasis.opendocument.text",
+    "application/pdf",
+    "application/vnd.ms-powerpoint",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    "application/rtf",
+    "text/plain",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/pdf"
+]
 
 validator = FileValidator(max_size=100000000, min_size=100, content_types=allowed_extensions)
 
+
 class FileConversion(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user")
-    input_file = models.FileField(upload_to=user_directory_path, validators=[validator,], storage=CleanFileNameStorage())
-    output_file = models.FileField(upload_to=lambda instance, filename: 'user_{0}/{1}'.format(instance.user.id, filename))
+    input_file = models.FileField(upload_to=user_directory_path, validators=[validator,], storage=CleanFileNameStorage(), unique=True)
+    output_file = models.FileField(upload_to=get_unique_filename)
     uploaded_at = models.DateTimeField(auto_now_add=True)
-
 
 #TODO: configure user specific upload areas
 # Maybe use https://stackoverflow.com/questions/34239877/django-save-user-uploads-in-seperate-folders
